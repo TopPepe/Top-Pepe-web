51a0e0a10f227586e1bf01a15b175c7e7003b6c1
z-btc <34449718+z-btc@users.noreply.github.com>
2023-05-25 11:51:37 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   const meta = {
-    title: 'Next.js Blog Starter Kit',
-    description: 'Clone and deploy your own Next.js portfolio in minutes.',
-    image: 'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
+    title: 'Top Pepe',
+    description: 'Escape the matrix.',
+    image: 'https://github.com/TopPepe/Top-Pepe-web/blob/main/public/images/warroom.png'
   }
 
   return (
